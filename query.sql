912e373dbcabb8e460f80e7be07073e0297f5350
Eniola Hannah <heniolahannah@gmail.com>
2025-01-25 21:30:36 +0100

    Standardizes Date_of_Booking to YYYY-MM-DD

--- query.sql
+++ query.sql
@@ -5,3 +5,11 @@ USE Hotel;
 SELECT * FROM hotel_dataset;
 
 SET SQL_SAFE_UPDATES = 0;
+
+-- DATA CLEANING TASKS: 
+-- Standardize Date_of_Booking to YYYY-MM-DD.        (Currently at m/d/y format)
+UPDATE hotel_dataset
+SET `Date of Booking` = STR_TO_DATE(`Date of Booking`, "%m/%d/%YYY");
+
+ALTER TABLE hotel_dataset
+MODIFY COLUMN `Date of Booking` DATE;
