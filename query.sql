391ad1de819b36361c3f19c01f68a3476d49553e
Eniola Hannah <heniolahannah@gmail.com>
2025-01-25 23:20:07 +0100

    Ensures that the column headers are in the right format majorly by renaming them

--- query.sql
+++ query.sql
@@ -17,3 +17,25 @@ MODIFY COLUMN `Date of Booking` DATE;
 -- Rename column headers to follow a consistent format (e.g., Date_of_Booking → Booking_Date)
 ALTER TABLE hotel_dataset
 RENAME COLUMN `Date of Booking` TO Booking_Date;
+
+-- Correct invalid entries in Age, Hotel_Rating, and Discount.
+-- No Invalid entries!
+
+-- Ensure that the column headers are in the right format. 
+ALTER TABLE hotel_dataset
+RENAME COLUMN `Booking ID` TO Booking_id;
+
+ALTER TABLE hotel_dataset
+RENAME COLUMN `Customer ID` TO Customer_id;
+
+ALTER TABLE hotel_dataset
+RENAME COLUMN `Origin Country` TO Origin_Country;
+
+ALTER TABLE hotel_dataset
+RENAME COLUMN `Hotel Name` TO Hotel_Name;
+
+ALTER TABLE hotel_dataset
+RENAME COLUMN `Hotel Rating` TO Hotel_Rating;
+
+ALTER TABLE hotel_dataset
+RENAME COLUMN `Booking Price[SGD]` TO `Booking_Price(SGD)`;
